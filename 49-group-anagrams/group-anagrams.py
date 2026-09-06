9126c26edc2579591440c1b5741f869d14dbc7f8
Harshitha S B <harshitha1656@gmail.com>
2026-09-06 20:28:48 +0530

    Time: 3 ms (100.00%) | Memory: 22.1 MB (64.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group={}
-        for word in strs:
-            key="".join(sorted(word))
-            if key not in group:
-                group[key]=[]
-            group[key].append(word)
-        return list(group.values())
-            
 
-        
+        freq={}
+        for word in strs:
+            key=" ".join(sorted(word))
+            if key not in freq:
+                freq[key]=[]
+            freq[key].append(word)
+        return list(freq.values())
