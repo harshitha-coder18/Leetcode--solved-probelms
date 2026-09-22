02dab2c752f3bf96769c54241133ddf161325ca6
Harshitha S B <harshitha1656@gmail.com>
2026-09-22 22:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (54.05%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        R = 0
+        L = 0
+        count = 0
+
+        for ch in s:
+            if ch == 'R':
+                R += 1
+            else:
+                L += 1
+
+            if R == L:
+                count += 1
+
+        return count
+
+        
